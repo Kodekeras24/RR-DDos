08c1c42f90cf60340fe14052b0d511ca4dd4e7bd
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-01 21:01:18 +0700

    Create ririe-ddos.py

---
+++ ririe-ddos.py
@@ -0,0 +1,39 @@
+import time
+import threading
+import socket
+import random
+import sys
+import os
+os.system("git pull")
+os.system("")
+
+# Colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    UNDERLINE = '\033[4m'
+    PURPLE = '\033[97m'
+    BOLD    = "\033[1m"
+    BLACK   = "\033[30m"
+    RED     = "\033[31m"
+    GREEN   = "\033[32m"
+    YELLOW  = "\033[33m"
+    BLUE    = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN    = "\033[36m"
+    WHITE   = "\033[37m"
+
+attemps = 0
+os.system("clear")
+print(" ")
+print("\033[94m    ±± ±± ±±      ±± ±± ±±                      ±±±             \033[0m")
+print("\033[94m    ±±       ±±   ±±       ±±                 ±± ±±             \033[0m")
+print("\033[94m    ±±       ±±   ±±       ±±                    ±±            \033[0m")
+print("\033[94m    ±±       ±±   ±±       ±±                    ±±           \033[0m")
+print("\033[97m    ±± ±± ±±      ±± ±± ±±        ________       ±±          \033[0m")
+print("\033[97m    ±±   ±±       ±±   ±±         ±± ±± ±±       ±±         \033[0m")
+print("\033[97m    ±±     ±±     ±±     ±±                      ±±        \033[0m")
+print("\033[97m    ±±       ±±   ±±       ±±                 ±± ±± ±±          \033[0m")
